81138bd6bcccd1111ef12cf620b4737757780b89
Адиль <bikeev1999@mail.ru>
2018-09-05 22:42:28 +0300

    Фиксирую изменения

--- Project_WPF/My_Project1/My_Project1/Properties/Settings.Designer.cs
+++ Project_WPF/My_Project1/My_Project1/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace My_Project1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,27 +20,6 @@ namespace My_Project1.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=172.16.80.228;user=username;database=shop;port=3306;password=password;")]
-        public string ConnString {
-            get {
-                return ((string)(this["ConnString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=172.16.80.228;user=username;database=my_company;port=3306;password=passwor" +
-            "d;")]
-        public string CoonStringPersonal {
-            get {
-                return ((string)(this["CoonStringPersonal"]));
             }
         }
     }
---
+++ Project_WPF/My_Project1/My_Project1/Window/Для заказчиков/FormalizeWin.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Windows.Documents;
+using System.Text.RegularExpressions;
+
+namespace My_Project1
+{
+    /// <summary>
+    /// Логика взаимодействия для FormalizeWin.xaml
+    /// </summary>
+    public partial class FormalizeWin : Window
+    {
+        public FormalizeWin()
+        {
+            InitializeComponent();
+        }
+
+        private void btFormalize_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbPhone_Validate())
+                try
+                {
+                    //TextRange textRange = new TextRange(rtbComment.Document.ContentStart, rtbComment.Document.ContentEnd);
+     
+
+                    //MySqlConnection conn = new MySqlConnection(My_Project1.Properties.Settings.Default.ConnString);
+                    //conn.Open();
+                    //if (tbFIO.Text == "" && tbVk.Text == "" && tbPhone.Text == "")
+                    //{
+                    //    MessageBox.Show("Заполните хотяб одно из полей ФИО/VK/Телефон, чтобы мы хоть как-то могли с вами связаться", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    //}
+                    //else
+                    //{
+                    //    MySqlCommand command = new MySqlCommand("INSERT INTO formalize_order (formalize_order.name_order, FIO, vk, phone, comment,progress_order) VALUES (\'" +
+                    //        tbNanmeOrder.Text + "\',\'"
+                    //        + tbFIO.Text + "\',\'"
+                    //        + tbVk.Text + "\',\'"
+                    //        + tbPhone.Text + "\',\'"
+                    //        + textRange.Text+ "\', 0)", conn);
+                    //    command.ExecuteNonQuery();
+                    //    conn.Close();
+                    //    MessageBox.Show("Заказ успешно оформлен !", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                    //    this.Close();
+                    //}
+                }catch(Exception exc)
+                {
+                    MessageBox.Show(exc.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+        }
+
+        private bool tbPhone_Validate()
+        {
+            try
+            {
+                Regex telephone = new Regex("[0-9]");
+                if (!telephone.IsMatch(tbPhone.Text))//проверяем введенную цифру на валидность
+                    MessageBox.Show("Номер телефона должен состоять только из цифр от 0 до 9 без символа \'+\' !", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Error);
+                else return true;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return false;
+        }
+    }
+}
